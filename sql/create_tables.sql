c8f84f303401a81c59d81a20b3f65ef275b667bf
4mbrus <ambrusfodor.online@gmail.com>
2025-12-16 18:44:14 +0100

    updated for new cols

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,7 +2,14 @@ CREATE EXTERNAL TABLE
 ambrus.fox_sentiment (
     title STRING,
     date DATE,
-    sentiment INT)
+    sentiment INT,
+    topic_inflation INT,
+    topix_taxes INT,
+    topic_stocks INT,
+    topic_jobs INT,
+    topic_housing INT,
+    topic_energy INT,
+    topic_crypto INT)
 ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
 LOCATION 's3://business-news-sentiments/news_sentiments/fox';
 
@@ -10,6 +17,13 @@ CREATE EXTERNAL TABLE
 ambrus.cnn_sentiment (
     title STRING,
     date DATE,
-    sentiment INT)
+    sentiment INT,
+    topic_inflation INT,
+    topix_taxes INT,
+    topic_stocks INT,
+    topic_jobs INT,
+    topic_housing INT,
+    topic_energy INT,
+    topic_crypto INT)
 ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
 LOCATION 's3://business-news-sentiments/news_sentiments/cnn';
